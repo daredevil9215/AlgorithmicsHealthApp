16f2862e03f888c3f0f30890cb57eb9c5492f293
daredevil9215 <grgo.lovric@gmail.com>
2023-07-07 20:14:42 +0200

    v 1.1

--- my_app.py
+++ my_app.py
@@ -1,65 +1,46 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-       QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout,
-       QGroupBox, QRadioButton,
-       QPushButton, QLabel, QListWidget, QLineEdit)
-
+       QApplication, QWidget, QVBoxLayout,
+       QPushButton, QLabel)
 
 from instr import *
 from second_win import *
-     
+
 class MainWin(QWidget):
-   def __init__(self):
-       ''' the window which the greeting is located in '''
-       super().__init__()
 
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
 
-       # creating and configuring graphic elements:
-       self.initUI()
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
 
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout)
+   
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
 
-       #establishes connections between elements
-       self.connects()
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
 
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
 
-       # sets what the window will look like (label, size, location)
-       self.set_appear()
+def main():
+    app = QApplication([])
+    mw = MainWin()
+    app.exec_()
 
-
-       # start:
-       self.show()
-
-
-   def initUI(self):
-       ''' creates graphic elements '''
-       self.btn_next = QPushButton(txt_next, self)
-       self.hello_text = QLabel(txt_hello)
-       self.instruction = QLabel(txt_instruction)
-
-
-       self.layout_line = QVBoxLayout()
-       self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-       self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
-       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
-       self.setLayout(self.layout_line)
-  
-   def next_click(self):
-       self.tw = TestWin()
-       self.hide()
-
-
-   def connects(self):
-       self.btn_next.clicked.connect(self.next_click)
-
-
-   ''' sets what the window will look like (label, size, location) '''
-   def set_appear(self):
-       self.setWindowTitle(txt_title)
-       self.resize(win_width, win_height)
-       self.move(win_x, win_y)
-
-
-app = QApplication([])
-mw = MainWin()
-app.exec_()
+main()
